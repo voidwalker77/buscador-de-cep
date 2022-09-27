545fc3ff0e687e203c05ff563f0c927c4cb31a0b
voidwalker <noccomposer@gmail.com>
2022-09-26 22:41:09 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { FiSearch } from 'react-icons/fi'
+import {GoLocation} from 'react-icons/go'
+import './styles.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import api from './services/api'
+
+export default function App() {
+	
+	const [input, setInput] = useState('')
+	const [cep, setCep] = useState({})
+	const regex =  /^[0-9]{5}-[0-9]{3}$/
+
+	const verification = (response) => {
+		if (Object.keys(response.data).length === 1) {
+			alert(`O Cep "${input.valueOf()}" não foi encontrado.\n Tente novamente ou busque outro CEP.`);
+		}
+	}
+
+	const handleSearch = async (e) => {
+		e.preventDefault()
+
+		try {
+			
+			if (input === '') {
+				alert('Preencha o campo com algum CEP!')
+			}
+			
+			const response = await api.get(`${input}/json`)	
+			setCep(response.data)
+			verification(response)
+			setInput('')	
+				
+		}
+		catch {
+			if (input.match(regex) !== regex) {
+				alert("Coloque um CEP válido.")
+			}
+			setInput('')
+		}
+
+	}	
+	
+	return (
+		<div className="container">
+			<h1 className="title">buscador cep</h1>
+
+			<div className="container--input">
+			<form>
+				<input
+					className="input--field" 
+					type="text" 
+					placeholder="digite seu cep..."
+					value = {input}
+					onChange = {(event) => setInput(event.target.value)}
+				/>
+				
+				<button className="button--search" onClick={handleSearch} type="submit">
+					<FiSearch size={25} color="white"/>
+				</button>
+			</form>
+			</div>
+			
+			{Object.keys(cep).length > 1 && (
+				<main className="main">
+					<h2><GoLocation size={30} color="5e0035"/> CEP: {cep.cep}</h2>
+
+					<span>Logradouro: {cep.logradouro}</span>
+					<span>Complemento: {cep.complemento}</span>
+					<span>Bairro: {cep.bairro}</span>
+					<span>Localidade: {cep.localidade} - {cep.uf}</span>
+
+				</main>
+			)}			
+		</div>
+	)
 }
-
-export default App;
